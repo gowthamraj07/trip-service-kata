104cbb05a22e6ebf18dd2925bf2aff7e25763ade
gowtham <gowthamraj07@gmail.com>
2020-02-28 14:41:19 +0100

    Should throw exception for invalid user

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,27 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class TripServiceTest {
-	
+
+    public static final User INVALID_USER = null;
+    public static final User GUEST = new User();
+
+    @Test
+    void should_throw_exception_when_user_not_logged_in() {
+        assertThrows(UserNotLoggedInException.class,()->{
+            new TestableTripService().getTripsByUser(GUEST);
+        });
+    }
+
+    private class TestableTripService extends TripService {
+        @Override
+        protected User getLoggedUser() {
+            return INVALID_USER;
+        }
+    }
 }
