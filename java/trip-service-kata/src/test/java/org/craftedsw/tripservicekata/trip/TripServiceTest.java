464e186de1503e44db2fa88700a0d26dcb9eeba1
gowtham <gowthamraj07@gmail.com>
2020-02-28 16:07:59 +0100

    Remove redundant test class

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -3,6 +3,7 @@ package org.craftedsw.tripservicekata.trip;
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.Collections;
 import java.util.List;
@@ -20,13 +21,13 @@ public class TripServiceTest {
     @Test
     void should_throw_exception_when_user_not_logged_in() {
         assertThrows(UserNotLoggedInException.class,()->{
-            new TestableTripService().getTripsByUser(STRANGER, INVALID_USER);
+            new TripService().getTripsByUser(STRANGER, INVALID_USER);
         });
     }
 
     @Test
     void should_not_return_trips_when_GUEST_logged_in() {
-        List<Trip> trips = new TestableTripService().getTripsByUser(STRANGER, LOGGED_IN_USER);
+        List<Trip> trips = new TripService().getTripsByUser(STRANGER, LOGGED_IN_USER);
 
         assertEquals(0,trips.size());
     }
@@ -35,25 +36,12 @@ public class TripServiceTest {
     void should_return_trips_when_FRIEND_logged_in() {
         List<Trip> expectedTrips = Collections.singletonList(new Trip());
         FRIEND.addFriend(LOGGED_IN_USER);
+        TripDAO tripDao = Mockito.mock(TripDAO.class);
+        Mockito.when(tripDao.findTripsBy(FRIEND)).thenReturn(expectedTrips);
 
-        List<Trip> actualTrips = new TestableTripService(expectedTrips).getTripsByUser(FRIEND, LOGGED_IN_USER);
+        List<Trip> actualTrips = new TripService(tripDao).getTripsByUser(FRIEND, LOGGED_IN_USER);
 
         assertEquals(expectedTrips,actualTrips);
     }
 
-    private class TestableTripService extends TripService {
-        private List<Trip> trips;
-
-        private TestableTripService() {
-        }
-
-        public TestableTripService(List<Trip> trips) {
-            this.trips = trips;
-        }
-
-        @Override
-        protected List<Trip> findTripsByUser(User user) {
-            return trips;
-        }
-    }
 }
