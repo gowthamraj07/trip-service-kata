c5a4cccda5f768f8b739599c2850a025f9bca667
gowtham <gowthamraj07@gmail.com>
2020-02-28 14:54:09 +0100

    Should not return trips when guest logged in

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -4,24 +4,40 @@ import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TripServiceTest {
 
     public static final User INVALID_USER = null;
     public static final User GUEST = new User();
+    private static final User LOGGED_IN_USER = new User();
 
     @Test
     void should_throw_exception_when_user_not_logged_in() {
         assertThrows(UserNotLoggedInException.class,()->{
-            new TestableTripService().getTripsByUser(GUEST);
+            new TestableTripService(INVALID_USER).getTripsByUser(GUEST);
         });
     }
 
+    @Test
+    void should_not_return_trips_when_GUEST_logged_in() {
+        List<Trip> trips = new TestableTripService(LOGGED_IN_USER).getTripsByUser(GUEST);
+
+        assertEquals(0,trips.size());
+    }
+
     private class TestableTripService extends TripService {
+        User invalidUser;
+
+        private TestableTripService(User invalidUser) {
+            this.invalidUser = invalidUser;
+        }
+
         @Override
         protected User getLoggedUser() {
-            return INVALID_USER;
+            return invalidUser;
         }
     }
 }
